edaf920e9aaeff8d387a8a0a085f4f524e123541
EC2 Default User <ec2-user@ip-172-31-10-232.ec2.internal>
2026-03-23 18:22:44 +0000

    lado do servidor atualizado com operacoes

--- server.py
+++ server.py
@@ -1,13 +1,46 @@
-from socket  import *
-from constCS import * #-
+from socket import *
+from constCS import *
 
-s = socket(AF_INET, SOCK_STREAM) 
-s.bind((HOST, PORT))  #-
-s.listen(1)           #-
-(conn, addr) = s.accept()  # returns new socket and addr. client 
-while True:                # forever
-  data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
-conn.close()               # close the connection
+s = socket(AF_INET, SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+
+print("Server is running...")
+
+(conn, addr) = s.accept()
+print("Connected by", addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+
+    msg = data.decode()
+    print("Received:", msg)
+
+    try:
+        parts = msg.split()
+        a = float(parts[0])
+        b = float(parts[1])
+        op = int(parts[2])
+
+        if op == 0:
+            result = a + b
+        elif op == 1:
+            result = a - b
+        elif op == 2:
+            result = a * b
+        elif op == 3:
+            if b == 0:
+                result = "Error: division by zero"
+            else:
+                result = a / b
+        else:
+            result = "Invalid operation"
+
+    except:
+        result = "Invalid input format"
+
+    conn.send(str(result).encode())
+
+conn.close()
