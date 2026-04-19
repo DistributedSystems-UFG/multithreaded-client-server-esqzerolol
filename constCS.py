01932ce4bc535a035adab39334c994e83f89d135
Samuel Amorim <68171967+esqzerolol@users.noreply.github.com>
2026-04-19 19:47:26 -0300

    Update constCS.py with server configuration
    Added server configuration with multiple server addresses.

--- constCS.py
+++ constCS.py
@@ -1,2 +1,14 @@
-HOST = '0.0.0.0'
+# constCS.py
+
+# Servidor
+HOST = "0.0.0.0"
 PORT = 5678
+#Altera esse número para cada servidor que você abrir
+
+# Lista de servidores que o cliente pode usar
+# Adicione quantos quiser aqui
+SERVERS = [
+    ("13.220.3.119", 5678),
+    ("3.239.30.117", 5679),
+    ("3.238.31.89", 5680),
+]
